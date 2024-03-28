2a7b132707c4b55950db8227d76f7ccd0c5a4146
jpreve02 <jimip2003@msn.com>
2024-03-28 08:53:51 -0400

    thursday328

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CatalogComponent,
-    SiteHeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
